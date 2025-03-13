d3393f9b99f872c40416b8875c8e14e21102e0dd
JuanZuniga79 <cd.juanzuniga@gmail.com>
2025-03-13 14:58:38 -0500

    initial config

---
+++ src/test/kotlin/com/example/backendcargotm/BackendCargoTmApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.backendcargotm
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendCargoTmApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
